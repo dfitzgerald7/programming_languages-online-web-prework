e8e3b1a3c1af27792b144c2b98c135ede67216a5
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-18 16:58:55 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,10 @@
+require "pry"
+
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
     hash.each do |language, hash2|
-      
+      binding.pry
       new_hash[language] = {hash2, :style => style}
     end 
   end 
