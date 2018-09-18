b20e01341ee358423dd646392ac877ed03858097
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-18 16:54:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
     hash.each do |language, hash2|
-      new_hash[language] = 
+      new_hash[language] = {}
     end 
   end 
 end
